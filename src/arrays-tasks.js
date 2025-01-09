e35262c439491f2781b3ad30af98c8362e8a584d
unknown <91miablond@gmail.com>
2025-01-09 17:19:32 +0700

    feat:solve task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -8,7 +8,7 @@
  ******************************************************************************************** */
 
 /**
- * Creates an array of integers from the specified start to end (inclusive).
+ * Ceatesr an array of integers from the specified start to end (inclusive).
  *
  * @param {number} start - The first number of an array.
  * @param {number} end - The last number of an array.
@@ -20,8 +20,8 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  return Array.from({ length: end - start + 1 }, (_, i) => start + i);
 }
 
 /**
